79a9707c1ac2d03875961b74c71b9fd263a0ff03
Erik Elze <erik@Eriks-MacBook-Pro.local>
2020-09-23 14:24:15 +0200

    edited

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,28 @@
 <template>
   <q-page class="flex flex-center bg-primary">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
+    <div class="q-pa-md"
+          style="width: 100%"
     >
+    <q-parallax>
+      <template v-slot:media>
+        <img class='bgImage' src="http://erikelze-events.de/images/banner.jpg">
+      </template>
+
+      <template v-slot:content="scope">
+        <div
+          class="absolute column items-center"
+          :style="{
+            opacity: 0.45 + (1 - scope.percentScrolled) * 0.55,
+            top: (scope.percentScrolled * 60) + '%',
+            left: 0,
+            right: 0
+          }"
+        >
+          <img src="https://erikelze-events.de/images/V3_Horizontal_white.png" style="width: 100%; height: auto">
+        </div>
+      </template>
+    </q-parallax>
+  </div>
   </q-page>
 </template>
 
@@ -12,3 +31,12 @@ export default {
   name: 'PageIndex'
 }
 </script>
+
+<style lang="scss">
+.bgImage:after {
+  background-image: -moz-linear-gradient(top, rgba(23, 24, 32, 0.90), rgba(23, 24, 32, 0.90)), url(https://erikelze-events.de/images/overlay.png);
+  background-image: -webkit-linear-gradient(top, rgba(23, 24, 32, 0.90), rgba(23, 24, 32, 0.90)), url(https://erikelze-events.de/images/overlay.png);
+  background-image: -ms-linear-gradient(top, rgba(23, 24, 32, 0.90), rgba(23, 24, 32, 0.90)), url(https://erikelze-events.de/images/overlay.png);
+  background-image: linear-gradient(top, rgba(23, 24, 32, 0.90), rgba(23, 24, 32, 0.90)), url(https://erikelze-events.de/images/overlay.png);
+}
+</style>
